d640e23a8b46d4f3cc1081ec3746f4819c219809
Diego Ramirez <dr01191115@gmail.com>
2024-12-14 12:35:38 -0600

    Primera idea de la edición para PC
    Asumiendo que soporta Tk/Tcl

---
+++ pc/main.py
@@ -0,0 +1,13 @@
+"Negociador (PC)"
+
+import tkinter
+import pillow
+
+# from src import ...
+
+VERSION = (1, 0, 0)
+
+if __name__ == "__main__":
+    mainTk = tkinter.Tk(screenName="Negociador")
+    # TODO: usar los módulos sobre "./src"
+    # para configurar esta ventana de tk
